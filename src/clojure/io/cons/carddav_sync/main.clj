85e0a42e2a695b4eb58579aeba0756ff64615f03
David Barksdale <amatus@amatus.name>
2013-08-26 20:31:42 -0500

    New lein-droid project.
    Created by: lein droid new carddav-sync io.cons.carddav_sync \
    		:target-sdk 18 :app-name CardDAV-Sync

---
+++ src/clojure/io/cons/carddav_sync/main.clj
@@ -0,0 +1,14 @@
+(ns io.cons.carddav_sync.main
+  (:use [neko.activity :only [defactivity set-content-view!]]
+        [neko.threading :only [on-ui]]
+        [neko.ui :only [make-ui]]
+        [neko.application :only [defapplication]]))
+
+(defactivity io.cons.carddav_sync.MainActivity
+  :def a
+  :on-create
+  (fn [this bundle]
+    (on-ui
+     (set-content-view! a
+      (make-ui [:linear-layout {}
+                [:text-view {:text "Hello from Clojure!"}]])))))

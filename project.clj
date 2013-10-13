7344795b8cce39f27f2864082ac930626cf00079
David Barksdale <amatus@amatus.name>
2013-10-13 14:42:24 -0500

    Update to lein-droid 0.2.0-preview1

--- project.clj
+++ project.clj
@@ -10,8 +10,8 @@
   :source-paths ["src/clojure" "src"]
   :java-source-paths ["src/java" "gen"]
 
-  :dependencies [[org.clojure-android/clojure "1.5.1-jb"]
-                 [neko/neko "3.0.0-beta6"]]
+  :dependencies [[org.clojure-android/clojure "1.5.1-jb" :use-resources true]
+                 [neko/neko "3.0.0-preview1"]]
   :profiles {:dev {:dependencies [[android/tools.nrepl "0.2.0-bigstack"]
                                   [compliment "0.0.2"]]
                    :android {:aot :all-with-unused}}
